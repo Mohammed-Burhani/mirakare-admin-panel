e976ff2c6a40325ba1ab63a9660e76db69123536
Mohammed-Burhani <jhansi@burhanitechnologies.com>
2025-12-19 17:27:27 +0530

    Added eslint ignoring concept

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
+  typescript: {
+    ignoreBuildErrors: true, // ✅ disables type checking at build time
+  },
+  eslint:{
+    ignoreDuringBuilds:true
+  }
 };
 
 export default nextConfig;
